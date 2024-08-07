fd93e478fe9791143c44cc22dd026c2ea21221c5
Gaurav Chaudhary <75659458+Gaurav-AL@users.noreply.github.com>
2024-08-07 14:45:22 +0530

    Spring Example

---
+++ src/main/java/com/tight/coupling/TightCouplingExample.java
@@ -0,0 +1,9 @@
+package com.tight.coupling;
+import com.tight.coupling.UserManager;
+
+public class TightCouplingExample {
+    public static void main(String[] args) {
+        UserManager userManager = new UserManager();
+        System.out.println(userManager.getUserInfo());
+    }
+}
